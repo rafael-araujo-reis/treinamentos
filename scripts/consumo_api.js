952a2e6ee1d09061b9f595f55f7ea5c89b095967
Rafael Reis <rafael.araujoreis@hotmail.com>
2021-06-01 06:45:46 -0300

    Change selected to list

--- scripts/consumo_api.js
+++ scripts/consumo_api.js
@@ -8,7 +8,7 @@ let URL_ESTADO = `${URL_BASE}${ESTADOS}${ORDER_BY}`;
 let respondeJSON = '';
 
 const comboEstados = document.querySelector('#comboEstados');
-const comboCidades = document.querySelector('#comboCidades');
+const listaCidades = document.querySelector('#listaCidades');
 
 const requestAPI = async (url) => {
   const response = await fetch(url, {
@@ -40,39 +40,39 @@ function carregarComboEstados() {
 
 comboEstados.addEventListener('change', () => {
   idEstado = comboEstados.options[comboEstados.selectedIndex].value;
-  atualizarComboCidades(idEstado);
+  atualizarListaCidades(idEstado);
 });
 
-function atualizarComboCidades(idEstado) {
+function atualizarListaCidades(idEstado) {
   let URL_CIDADE = `${URL_BASE}${ESTADOS}/${idEstado}${CIDADES}${ORDER_BY}`;
 
   requestAPI(URL_CIDADE)
     .then(data => cidadesJSON = data);
 
   setTimeout(() => {
-    carregarComboCidades();
+    carregarListaCidades();
   }, 2000);
 }
 
-function carregarComboCidades() {
-  comboCidades.innerHTML = '';
+function carregarListaCidades() {
+  listaCidades.innerHTML = '';
   for (let index = 0; index < cidadesJSON.length; index++) {
     const cidade = cidadesJSON[index];
     const id = cidade.id;
     const value = cidade.id;
     const nome = cidade.nome;
 
-    const option = document.createElement('option');
-    option.setAttribute('id', id);
-    option.setAttribute('value', value);
-    option.innerHTML = nome;
+    const li = document.createElement('li');
+    li.setAttribute('id', id);
+    li.setAttribute('value', value);
+    li.innerHTML = nome;
 
-    comboCidades.append(option);
+    listaCidades.append(li);
   }
 }
 
 setTimeout(() => {
   carregarComboEstados();
   idEstado = comboEstados.options[comboEstados.selectedIndex].value;
-  atualizarComboCidades(idEstado);
+  atualizarListaCidades(idEstado);
 }, 1000);
